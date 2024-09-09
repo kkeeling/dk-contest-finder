20ef8804d6c77003df874e813be7c77e1b8d7215
Keenan Keeling <kkeeling@automatastudios.com>
2024-09-08 20:25:52 -0400

    refactor: Update scheduler to run contest finder every 5 minutes

--- src/scheduler.py
+++ src/scheduler.py
@@ -29,8 +29,8 @@ class Scheduler:
 
     def start(self):
         self.is_running = True
-        schedule.every(30).minutes.do(self.run_contest_finder)
-        logger.info("Scheduler started. Running contest finder every 30 minutes.")
+        schedule.every(5).minutes.do(self.run_contest_finder)
+        logger.info("Scheduler started. Running contest finder every 5 minutes.")
         
         # Run the contest finder immediately on start
         self.run_contest_finder()
